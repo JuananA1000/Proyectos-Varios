83faf091e4d2e1c21731b3bdf2956ffb15f2591a
Juanan <juanantonioamillopez@gmail.com>
2023-11-28 11:59:30 +0100

    Main codes

--- JuANANtify/main.js
+++ JuANANtify/main.js
@@ -1,1 +1,13 @@
-// Aquí tu js
+const image = document.getElementById('cover');
+const title = document.getElementById('music-title');
+const artist = document.getElementById('music-artist');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
+const progress = document.getElementById('progress');
+const playerProgress = document.getElementById('player-progress');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const playBtn = document.getElementById('play');
+const background = document.getElementById('bg-img');
+
+const music = new Audio()
