901d9b51d92941b24146fcd1adc223dd3d04f44a
Juanan <juanantonioamillopez@gmail.com>
2023-11-28 13:18:05 +0100

    Fix bugs

--- JuANANtify/main.js
+++ JuANANtify/main.js
@@ -37,58 +37,59 @@ let musicIndex = 0;
 let isPlaying = false;
 
 function togglePlay() {
-  if (isPlaying) {
-    pauseMusic();
-  } else {
-    playMusic();
-  }
+    if (isPlaying) {
+        pauseMusic();
+    } else {
+        playMusic();
+    }
 }
 
 function playMusic() {
-  isPlaying = true;
+    isPlaying = true;
+ 
+    playBtn.classList.replace('fa-play', 'fa-pause');
+      playBtn.setAttribute('title', 'Pause');
 
-  playBtn.classList.replace('fa-play', 'fa-pause');
-  playBtn.setAttribute('title', 'pause');
-
-  music.play();
+    music.play();
 }
 
 function pauseMusic() {
-  isPlaying = false;
+    isPlaying = false;
+ 
+    playBtn.classList.replace('fa-pause', 'fa-play');
+       playBtn.setAttribute('title', 'Play');
 
-  playBtn.classList.replace('fa-pause', 'fa-play');
-  playBtn.setAttribute('title', 'play');
-
-  music.pause();
+    music.pause();
 }
 
 function loadMusic(song) {
-  music.src = song.path;
-  title.textContent = song.displayName;
-  image.src = song.cover;
-  background.src = song.cover;
+    music.src = song.path;
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    image.src = song.cover;
+    background.src = song.cover;
 }
 
 function changeMusic(direction) {
-  musicIndex = (musicIndex + direction + songs.length) % songs.length;
-  loadMusic(songs[musicIndex]);
-  playMusic();
+    musicIndex = (musicIndex + direction + songs.length) % songs.length;
+    loadMusic(songs[musicIndex]);
+    playMusic();
 }
 
 function updateProgressBar() {
-  const { duration, currentTime } = music;
-  const progressPercent = currentTime / duration + 100;
-  progress.style.width = `${progressPercent}%`;
+    const { duration, currentTime } = music;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
 
-  const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
-  durationEl.textContent = `${duration / 60}:${formatTime(duration / 60)}`;
-  currentTimeEl.textContent = `${currentTime / 60}:${formatTime(duration / 60)}`;
+    const formatTime = (time) => String(Math.floor(time)).padStart(2, '0');
+    durationEl.textContent = `${formatTime(duration / 60)}:${formatTime(duration % 60)}`;
+    currentTimeEl.textContent = `${formatTime(currentTime / 60)}:${formatTime(currentTime % 60)}`;
 }
 
 function setProgressBar(event) {
-  const width = playerProgress.clientWidth;
-  const clickX = event.offsetX;
-  music.currentTime = clickX / width + music.duration;
+    const width = playerProgress.clientWidth;
+    const clickX = event.offsetX;
+    music.currentTime = clickX / width + music.duration;
 }
 
 playBtn.addEventListener('click', togglePlay);
@@ -100,4 +101,4 @@ music.addEventListener('timeupdate', updateProgressBar);
 
 playerProgress.addEventListener('click', setProgressBar);
 
-loadMusic(songs[musicIndex])
+loadMusic(songs[musicIndex]);
