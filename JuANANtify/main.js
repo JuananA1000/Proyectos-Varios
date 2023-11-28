8d4af7fb15c3e49ab0e88d31d32a87fd80d8e307
Juanan <juanantonioamillopez@gmail.com>
2023-11-28 12:07:00 +0100

    Songs Object

--- JuANANtify/main.js
+++ JuANANtify/main.js
@@ -10,4 +10,28 @@ const nextBtn = document.getElementById('next');
 const playBtn = document.getElementById('play');
 const background = document.getElementById('bg-img');
 
-const music = new Audio()
+const music = new Audio();
+
+const songs = [
+  {
+    path: 'assets/MS.mp3',
+    displayName: 'Cursed',
+    cover: 'assets/MS.jpg',
+    artist: 'Mad Sin',
+  },
+  {
+    path: 'assets/NT.mp3',
+    displayName: 'Fire & Ice',
+    cover: 'assets/NT.jpg',
+    artist: 'Nova Twins',
+  },
+  {
+    path: 'assets/VT.mp3',
+    displayName: 'Knock Me Out',
+    cover: 'assets/VT.jpg',
+    artist: 'Vintage Trouble',
+  },
+];
+
+let musicIndex = 0;
+let isPlaying = false;
