bcaaadaa7ead8f4a1726d4008d044f5d3a472ea5
Juanan <juanantonioamillopez@gmail.com>
2023-12-01 12:34:07 +0100

    Arreglar bugs

--- CaraOCruz/main.js
+++ CaraOCruz/main.js
@@ -5,18 +5,18 @@ let flipBtn = document.querySelector('#flip-button');
 let resetBtn = document.querySelector('#reset-button');
 
 flipBtn.addEventListener('click', () => {
-  let i = Math.floor(Math.random() + 2);
+  let i = Math.floor(Math.random() * 2);
   coin.style.animation = 'none';
 
   if (i) {
     setTimeout(function () {
       coin.style.animation = 'spin-heads 3s forwards';
-    });
+    }, 100);
     heads++;
   } else {
     setTimeout(function () {
       coin.style.animation = 'spin-tails 3s forwards';
-    });
+    }, 100);
     tails++;
   }
   setTimeout(updateStats, 3000);
@@ -24,8 +24,8 @@ flipBtn.addEventListener('click', () => {
 });
 
 function updateStats() {
-  document.querySelector('#heads-count').textContent = `Heads: ${heads}`;
-  document.querySelector('#tails-count').textContent = `Tails: ${tails}`;
+  document.querySelector('#heads-count').textContent = `Caras: ${heads}`;
+  document.querySelector('#tails-count').textContent = `Cruces: ${tails}`;
 }
 
 function disableButton() {
