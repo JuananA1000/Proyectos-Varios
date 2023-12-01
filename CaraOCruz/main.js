3103f1dbfbab3cd7028ea797abdee54b1984f550
Juanan <juanantonioamillopez@gmail.com>
2023-12-01 12:18:41 +0100

    DisableButton function

--- CaraOCruz/main.js
+++ CaraOCruz/main.js
@@ -27,3 +27,8 @@ function updateStats() {
   document.querySelector('#heads-count').textContent = `Heads: ${heads}`;
   document.querySelector('#tails-count').textContent = `Tails: ${tails}`;
 }
+
+function disableButton() {
+  flipBtn.disabled = true;
+  setTimeout(() => (flipBtn.disabled = false), 3000);
+}
