04b4775a51d7b34576474955a6111dd894932d3e
Juanan <juanantonioamillopez@gmail.com>
2023-12-04 14:06:10 +0100

    ToggleDarkMode function

--- Skeleton/main.js
+++ Skeleton/main.js
@@ -1,3 +1,9 @@
-const allLoadings= document.querySelectorAll('.loading')
-const toggle= document.getElementById('toggle')
+const allLoadings = document.querySelectorAll('.loading');
+const toggle = document.getElementById('toggle');
 
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+
+  const isDarkModeEnabled = document.body.classList.contains('dark-mode');
+  localStorage.setItem('darkmode', isDarkModeEnabled);
+}
