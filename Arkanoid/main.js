4e3a49bc97ddf3889273e59c427e0f151b45436c
Juanan <juanantonioamillopez@gmail.com>
2023-12-05 13:01:51 +0100

    Variables principales

---
+++ Arkanoid/main.js
@@ -0,0 +1,25 @@
+let canvas = document.getElementById('game');
+let ctx = canvas.getContext('2D');
+
+let ballRadius = 9;
+let x = canvas.width / (Math.floor(Math.random() * Math.random() * 10) + 3);
+let y = canvas.height - 40;
+let dx = 2;
+let dy = -2;
+
+let paddleHeight = 12;
+let paddleWidth = 72;
+
+// Posición de inicio de la barra
+let paddleX = (canvas.width - paddleWidth) / 2;
+
+// Ladrillos
+let rowCount = 5;
+let columnCount = 9;
+let brickWidth = 54;
+let brickHeight = 18;
+let brickPadding = 12;
+let topOffset = 40;
+let leftOffset = 33;
+
+let score = 0;
