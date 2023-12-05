20798c487e47ca547b5116b9abcd0f6221c05011
Juanan <juanantonioamillopez@gmail.com>
2023-12-05 13:20:32 +0100

    Dibujar bola

--- Arkanoid/main.js
+++ Arkanoid/main.js
@@ -53,3 +53,12 @@ function drawPaddle() {
   ctx.fill();
   ctx.closePath();
 }
+
+// Dibujar bola
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = '#33333';
+  ctx.fill();
+  ctx.closePath();
+}
