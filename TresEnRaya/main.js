8fab079b2fda9397298c5997aa7aa5c072c8fab9
Juanan <juanantonioamillopez@gmail.com>
2023-12-26 09:38:13 +0100

    save

--- TresEnRaya/main.js
+++ TresEnRaya/main.js
@@ -3,10 +3,14 @@ playerTurn = 'x';
 moves = 0;
 isGameOver = false;
 span = document.getElementsByTagName('span');
-restartButton =
-  '<button onclick="playAgain()"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16"><path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/><path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/></svg></button>';
+restartButton = `
+  <button onclick="playAgain()">
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-repeat" viewBox="0 0 16 16">
+      <path d="M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z"/><path fill-rule="evenodd" d="M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z"/>
+    </svg>
+  </button>
+`;
 
-  
 function play(y) {
   if (y.dataset.player == 'none' && window.isGameOver == false) {
     y.innerHTML = playerTurn;
@@ -19,8 +23,7 @@ function play(y) {
     }
   }
 
-  /* Win Types */
-
+  // Chequear victorias
   checkWinner(1, 2, 3);
   checkWinner(4, 5, 6);
   checkWinner(7, 8, 9);
@@ -30,8 +33,7 @@ function play(y) {
   checkWinner(1, 5, 9);
   checkWinner(3, 5, 7);
 
-  /* No Winner */
-
+  // Empate
   if (moves == 9 && isGameOver == false) {
     draw();
   }
@@ -73,9 +75,12 @@ function resetGame() {
 }
 
 function gameOver(a) {
-  var gameOverAlertElement =
-    '<b>GAME OVER </b><br><br> Player ' + span[a].dataset.player.toUpperCase() + ' Win !!! <br><br>' + restartButton;
-  var div = document.createElement('div');
+  const gameOverAlertElement =
+    '<b>GAME OVER </b><br><br> El jugador ' +
+    span[a].dataset.player.toUpperCase() +
+    ' Gana!!! <br><br>' +
+    restartButton;
+  const div = document.createElement('div');
   div.className = 'alert';
   div.innerHTML = gameOverAlertElement;
   document.getElementsByTagName('body')[0].appendChild(div);
@@ -84,8 +89,8 @@ function gameOver(a) {
 }
 
 function draw() {
-  var drawAlertElement = '<b>DRAW!!!</b><br><br>' + restartButton;
-  var div = document.createElement('div');
+  const drawAlertElement = '<b>EMPATE</b><br><br>' + restartButton;
+  const div = document.createElement('div');
   div.className = 'alert';
   div.innerHTML = drawAlertElement;
   document.getElementsByTagName('body')[0].appendChild(div);
