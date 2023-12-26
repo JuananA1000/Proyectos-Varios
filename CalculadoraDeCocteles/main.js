7ad76e3a64ac506430026018e87eb90b1d9a8372
Juanan <juanantonioamillopez@gmail.com>
2023-12-26 14:04:44 +0100

    Variables principales

--- CalculadoraDeCocteles/main.js
+++ CalculadoraDeCocteles/main.js
@@ -1,1 +1,8 @@
-// Aquí tu js
+/* 
+  IDEA: El cóctel resultado es un array vacío al que se le añaden ingredientes, si existe, aparece la foto del cóctel, 
+  si no, aparece otra cosa, ya veré cual
+*/
+
+const ingredientes = document.querySelectorAll('img');
+const btnShake = document.querySelector('.btnShake');
+let coctel = [];
