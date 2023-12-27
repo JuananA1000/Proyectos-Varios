c2305139ebf39f264ea8a70e2345f705e7445a0c
Juanan <juanantonioamillopez@gmail.com>
2023-12-27 09:25:54 +0100

    Añadir ingrediente

--- CalculadoraDeCocteles/main.js
+++ CalculadoraDeCocteles/main.js
@@ -1,24 +1,27 @@
-/* 
-  IDEA: El cóctel resultado es un array vacío al que se le añaden ingredientes, si existe, aparece la foto del cóctel, 
-  si no, aparece otra cosa, ya veré cual
-*/
-
 const ingredientes = document.querySelectorAll('img');
 const btnShake = document.querySelector('.btnShake');
 const coctel = [];
 
-ingredientes.forEach(function (img) {
-  img.addEventListener('click', añadirIngrediente);
-});
+ingredientes.forEach((img) => img.addEventListener('click', añadirIngrediente));
 btnShake.addEventListener('click', crearCoctel);
 
-// Función para añadir ingredientes a la receta
+// Añadir ingredientes a la receta
 function añadirIngrediente(event) {
-  const titulo = event.target.getAttribute('title');
-  console.log(titulo);
+  const ingrediente = event.target.getAttribute('title');
+  const resultado = document.querySelector('.resultado');
+
+  if (ingrediente && !coctel.includes(ingrediente)) {
+    coctel.push(ingrediente);
+    resultado.innerHTML = coctel.join(' + ');
+  } else {
+    alert('repetido');
+  }
+
+  // PENDIENTE: Borrar
+  console.log('Este cóctel lleva: ', coctel);
 }
 
-// Función para agitar el cóctel, asignada al botón 'shake'
+// Agitar el cóctel, asignada al botón 'shake'
 function crearCoctel() {
   console.log('Crear coctel');
 }
