636df30af6d3d0b7ff51c032e801c49c9f7e6641
Juanan <juanantonioamillopez@gmail.com>
2023-12-27 10:35:10 +0100

    Crear cóctel

--- CalculadoraDeCocteles/main.js
+++ CalculadoraDeCocteles/main.js
@@ -14,14 +14,34 @@ function añadirIngrediente(event) {
     coctel.push(ingrediente);
     resultado.innerHTML = coctel.join(' + ');
   } else {
+    // PENDIENTE: alert personalizado
     alert('repetido');
   }
-
-  // PENDIENTE: Borrar
-  console.log('Este cóctel lleva: ', coctel);
 }
 
 // Agitar el cóctel, asignada al botón 'shake'
 function crearCoctel() {
-  console.log('Crear coctel');
+  if (coctel.length > 0) {
+    if (
+      coctel.includes('Lima') &&
+      coctel.includes('Agua con Gas') &&
+      coctel.includes('Ron') &&
+      coctel.includes('Azúcar')
+    ) {
+      console.log('Mojito');
+    } else if (
+      coctel.includes('Azúcar') &&
+      coctel.includes('Agua con Gas') &&
+      coctel.includes('Angostura') &&
+      coctel.includes('Wiski')
+    ) {
+      console.log('Old Fashioned');
+    } else if (coctel.includes('Lima') && coctel.includes('Azúcar') && coctel.includes('Cachaza')) {
+      console.log('Caipirinha');
+    } else {
+      console.log('Ese cóctel no esite');
+    }
+  } else {
+    console.log('Coctelera vacía');
+  }
 }
