dc9e25b6b017c01e398451aadb228c8ab6729445
Juanan <juanantonioamillopez@gmail.com>
2023-12-27 10:45:35 +0100

    Limpiar pantalla al agitar y eliminar tareas pendientes

--- CalculadoraDeCocteles/main.js
+++ CalculadoraDeCocteles/main.js
@@ -1,26 +1,33 @@
 const ingredientes = document.querySelectorAll('img');
 const btnShake = document.querySelector('.btnShake');
+const resultado = document.querySelector('.resultado');
 const coctel = [];
 
 ingredientes.forEach((img) => img.addEventListener('click', añadirIngrediente));
 btnShake.addEventListener('click', crearCoctel);
 
+// PENDIENTE: 1. eliminar console.logs, que aparezca la imagen del coctel en la pantalla
+// PENDIENTE: 2. alerts personalizados
+// PENDIENTE: 3. la app es una coctelería, darle un estilo más guasón y profesional
+// PENDIENTE: 4. añadir más ingredientes y más cócteles
+// IDEA: al pulsar 'shake', queremos que la pantalla vibre un poco, y que el cóctel salga a los 3 segundos
+
 // Añadir ingredientes a la receta
 function añadirIngrediente(event) {
   const ingrediente = event.target.getAttribute('title');
-  const resultado = document.querySelector('.resultado');
 
   if (ingrediente && !coctel.includes(ingrediente)) {
     coctel.push(ingrediente);
     resultado.innerHTML = coctel.join(' + ');
   } else {
-    // PENDIENTE: alert personalizado
     alert('repetido');
   }
 }
 
 // Agitar el cóctel, asignada al botón 'shake'
 function crearCoctel() {
+  resultado.innerHTML = '';
+
   if (coctel.length > 0) {
     if (
       coctel.includes('Lima') &&
