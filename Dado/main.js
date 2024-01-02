07f7a131777d48fe203f8b6a280ef090db1215cc
Juanan <juanantonioamillopez@gmail.com>
2024-01-02 15:02:48 +0100

    Función randomDice

--- Dado/main.js
+++ Dado/main.js
@@ -1,2 +1,12 @@
 const dice = document.querySelector('.dice');
 const rollBtn = document.querySelector('.roll');
+
+function randomDice() {
+  const random = Math.floor(Math.random() * 10);
+
+  if (random >= 1 && random <= 6) {
+    rollDice(random);
+  } else {
+    rollDice();
+  }
+}
