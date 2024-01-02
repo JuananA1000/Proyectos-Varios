c9645c11d7a06b4eed1353b1c540cf96b6b81fe3
Juanan <juanantonioamillopez@gmail.com>
2024-01-02 17:29:12 +0100

    Función rollDice

--- Dado/main.js
+++ Dado/main.js
@@ -10,3 +10,34 @@ function randomDice() {
     rollDice();
   }
 }
+
+function rollDice(random) {
+  dice.style.animation = 'rolling 4s';
+
+  setTimeout(() => {
+    switch (random) {
+      case 1:
+        dice.style.transform = 'rotateX(0deg) rotateY(0deg)';
+        break;
+      case 2:
+        dice.style.transform = 'rotateX(-90deg) rotateY(0deg)';
+        break;
+      case 3:
+        dice.style.transform = 'rotateX(0deg) rotateY(90deg)';
+        break;
+      case 4:
+        dice.style.transform = 'rotateX(0deg) rotateY(-90deg)';
+        break;
+      case 5:
+        dice.style.transform = 'rotateX(90deg) rotateY(0deg)';
+        break;
+      case 6:
+        dice.style.transform = 'rotateX(100deg) rotateY(0deg)';
+        break;
+
+      default:
+        break;
+    }
+    dice.style.animation = 'none';
+  }, 4050);
+}
