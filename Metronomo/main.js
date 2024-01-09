c2c82fc3267ca61350c4eabe5b88c372dfff8d4e
Juanan <juanantonioamillopez@gmail.com>
2024-01-09 11:00:38 +0100

    Función play manual

--- Metronomo/main.js
+++ Metronomo/main.js
@@ -1,9 +1,14 @@
-// Aquí tu js
 const menosBtn = document.getElementById('menos-btn');
 const masBtn = document.getElementById('mas-btn');
+const playBtn = document.getElementById('play-btn');
+const clicks = document.querySelectorAll('.click');
+const audio = document.querySelector('audio');
+
+let clickActual = 0;
 
 masBtn.addEventListener('click', aumentarBpm);
 menosBtn.addEventListener('click', disminuirBpm);
+playBtn.addEventListener('click', play);
 
 function aumentarBpm() {
   let velocidad = document.querySelector('.velocidad');
@@ -24,5 +29,24 @@ function disminuirBpm() {
   if (!isNaN(numero)) {
     let nuevoNumero = numero - 1;
     velocidad.innerText = nuevoNumero + ' bpm';
+  }
+}
+
+function play() {
+  let clickAnterior = clickActual - 1;
+
+  if (clickActual < clicks.length) {
+    if (clickActual > 0) {
+      clicks[clickAnterior].classList.remove('clicked');
+    }
+    clicks[clickActual].classList.add('clicked');
+    audio.play();
+    clickActual++;
+  } else {
+    clicks[clickAnterior].classList.remove('clicked');
+    clickActual = 0;
+    clicks[clickActual].classList.add('clicked');
+    audio.play();
+    clickActual++;
   }
 }
