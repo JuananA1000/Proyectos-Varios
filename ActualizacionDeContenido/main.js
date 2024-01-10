0262765a579425a1599bf9c4bda6a09f5a47e715
Juanan <juanantonioamillopez@gmail.com>
2024-01-10 09:53:20 +0100

    Traductor

--- ActualizacionDeContenido/main.js
+++ ActualizacionDeContenido/main.js
@@ -5,7 +5,7 @@ const API_URL = 'https://api.chucknorris.io/jokes/random';
 let intervalId; // Variable para almacenar el ID del intervalo
 
 playBtn.addEventListener('click', () => (intervalId = setInterval(obtenerFrase, 1000)));
-stopBtn.addEventListener('click', stop); // Detener el intervalo usando clearInterval y pasando el identificador
+stopBtn.addEventListener('click', stop);
 
 function obtenerFrase() {
   fetch(API_URL)
@@ -16,8 +16,13 @@ function obtenerFrase() {
       return respuesta.json();
     })
     .then((data) => {
-      console.log('DATA: ', data.value);
-      frase.innerHTML = `${data.value}`;
+      traducirTexto(data.value, 'en', 'es').then((traduccion) => {
+        if (traduccion) {
+          frase.innerHTML = `${traduccion}`;
+        } else {
+          frase.innerHTML = 'No se pudo traducir la frase.';
+        }
+      });
     })
     .catch((error) => {
       console.error('Error:', error);
@@ -26,5 +31,23 @@ function obtenerFrase() {
 
 function stop() {
   frase.innerHTML = 'Frase';
-  clearInterval(intervalId)
+  clearInterval(intervalId); // Detener el intervalo usando clearInterval y pasando el identificador
+}
+
+async function traducirTexto(texto, idiomaOrigen, idiomaDestino) {
+  const TRANSLATE_URL = `https://api.mymemory.translated.net/get?q=${texto}&langpair=${idiomaOrigen}|${idiomaDestino}`;
+
+  try {
+    const respuesta = await fetch(TRANSLATE_URL);
+    const data = await respuesta.json();
+
+    if (data && data.responseStatus === 200) {
+      return data.matches[0].translation;
+    } else {
+      throw new Error('Error en la traducción');
+    }
+  } catch (error) {
+    console.error('Hubo un error al traducir:', error);
+    return null;
+  }
 }
