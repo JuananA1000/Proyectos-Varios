ef6cdf3e66078b0bd7d0dfccb5b50cd1a4b67bc1
Juanan <juanantonioamillopez@gmail.com>
2024-01-10 09:50:45 +0100

    Función stop

--- ActualizacionDeContenido/main.js
+++ ActualizacionDeContenido/main.js
@@ -1,9 +1,11 @@
 const playBtn = document.querySelector('.play-btn');
+const stopBtn = document.querySelector('.stop-btn');
 const frase = document.querySelector('.frase');
 const API_URL = 'https://api.chucknorris.io/jokes/random';
 let intervalId; // Variable para almacenar el ID del intervalo
 
-playBtn.addEventListener('click', () => (intervalId = setInterval(obtenerFrase, 2000)));
+playBtn.addEventListener('click', () => (intervalId = setInterval(obtenerFrase, 1000)));
+stopBtn.addEventListener('click', stop); // Detener el intervalo usando clearInterval y pasando el identificador
 
 function obtenerFrase() {
   fetch(API_URL)
@@ -20,4 +22,9 @@ function obtenerFrase() {
     .catch((error) => {
       console.error('Error:', error);
     });
+}
+
+function stop() {
+  frase.innerHTML = 'Frase';
+  clearInterval(intervalId)
 }
