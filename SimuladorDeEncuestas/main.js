d3ee795b084872c8f62cc6475a69a36f1b72ec7c
Juanan <juanantonioamillopez@gmail.com>
2024-01-11 12:26:32 +0100

    Función cambiar de pregunta

--- SimuladorDeEncuestas/main.js
+++ SimuladorDeEncuestas/main.js
@@ -1,1 +1,18 @@
-// Aquí tu js
+const preguntas = document.querySelectorAll('.pregunta');
+const cuentaAtras = document.querySelectorAll('.cuenta-atras');
+const anterior = document.getElementById('btn-anterior');
+const siguiente = document.getElementById('btn-siguiente');
+
+siguiente.addEventListener('click', sigPregunta);
+
+function sigPregunta() {
+  for (let i = 0; i < preguntas.length; i++) {
+    if (!preguntas[i].classList.contains('oculto')) {
+      preguntas[i].classList.add('oculto');
+      let siguiente = (i + 1) % preguntas.length;
+      preguntas[siguiente].classList.remove('oculto');
+      break;
+    }
+  }
+  console.log('Siguiente');
+}
