8453419a672af45eae0d8536a259e1176ea411bd
Juanan <juanantonioamillopez@gmail.com>
2024-02-01 11:31:51 +0100

    Corregir cálculo de dias y añadir favicon

--- DiasParaEvento/main.js
+++ DiasParaEvento/main.js
@@ -8,23 +8,31 @@ botonAdd.addEventListener('click', addEvento);
 
 function addEvento(e) {
   e.preventDefault();
-  
+
   let tarea = document.createElement('div');
- 
-  tarea.innerHTML = `
-    <div class="tarea">
-      <div class="fecha">${fechaEvento.value} día(s)</div>
-      <div class="nombre">${nombreEvento.value}</div>
-      
-      <div class="acciones">
-        <button id="eliminar">Eliminar</button>
+
+  let fechaHoy = new Date();
+  let difFechas = new Date(fechaEvento.value) - fechaHoy.getTime();
+  let diasParaEvento = Math.ceil(difFechas / (1000 * 3600 * 24));
+
+  if (nombreEvento.value !== '') {
+    tarea.innerHTML = `
+      <div class="tarea">
+        <div class="fecha" title=${fechaEvento.value}>${diasParaEvento} día(s)</div>
+        <div class="nombre">${nombreEvento.value}</div>
+        
+        <div class="acciones">
+          <button id="eliminar">Eliminar</button>
+        </div>
       </div>
-    </div>
-  `;
+    `;
+  } else {
+    alert('Introduce título de evento');
+  }
 
   contenedorTareas.appendChild(tarea);
-  fechaEvento.value = '';
   nombreEvento.value = '';
+  fechaEvento.value = '';
 
   const botonEliminar = tarea.querySelector('#eliminar');
 
